c51b2529e9c1ab3a08907e89d4bb73ec29f38a21
Mert Urhan <mert-urhan@hotmail.com>
2023-03-20 22:25:16 +0300

    HW1.2 Finished
    ./19050111067 5 6 op.txt

--- HW1/19050111067.c
+++ HW1/19050111067.c
@@ -1,6 +1,69 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <hellomake.h>
 
-int main(){
-	myFunc();
+int main(int argc, char *argv[]){
+	//myFunc();
+
+	if(argc != 4){
+		printf("Error! Give four arguments\n");
+		printf("arraysize1 arraysize2 fileName");
+		return 1;
+	}
+	
+	int x= atoi(argv[1]);
+	int y= atoi(argv[2]);
+	char *fileName = argv[3];
+
+	long int seed = 19050111067;
+	double *vector;
+	double **matrix;
+
+	int i, j;
+
+	matrix = (double **)malloc(x * sizeof(double *));
+
+	for(i=0; i<x; i++){
+		matrix[i] = (double *)malloc(y * sizeof(double));
+	}
+
+	srand(seed);
+
+	for(i=0; i<x; i++){
+		for(j=0; j<y; j++){
+			matrix[i][j] = (rand()) % 100 + 1.0;
+		}
+	}
+	printf("%dx%d matrix defined and filled with random numbers", x, y);
+
+
+	vector = (double *)malloc(y * sizeof(double *));
+	for(i=0; i<y; i++){
+		vector[i] = (rand()) % 100 + 1.0;
+	}
+	printf("Random vector created for matrix multiplication\n");
+
+
+	FILE *fp = fopen(fileName, "w");
+
+	if(fp == NULL){
+		printf("%s couldn't find", fileName);
+	}
+
+	double result = 0;
+	for(i=0; i<x; i++){
+		for(j=0; j<y; j++){
+			result = matrix[i][j] *  vector[j];
+		}
+		fprintf(fp, "%.lf\n", result);
+		result = 0;
+	}
+
+	printf("\nMultiplied and wrote to %s txt\n\n", fileName);
+	fclose(fp);
+
+
 	return 0;
+
 }
